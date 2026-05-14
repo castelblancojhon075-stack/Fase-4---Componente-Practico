7620262c9e0960990edfc20c14d99ce70302f795
Jhon Castelblanco <castelblancojhon075@gmail.com>
2026-05-14 01:02:43 -0500

    Se mejoran los  logs del sistema y los registro de los  eventos_Jhon Castelblanco

--- Gestor.py
+++ Gestor.py
@@ -2,29 +2,55 @@ import logging
 
 # Configuración del archivo de logs
 
+import logging
+
 logging.basicConfig(
     filename="logs.txt",
-    level=logging.ERROR,
+    level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
-
-# Clase encargada de administrar el sistema
 
 class GestorSistema:
 
     def __init__(self):
+
+        # Lista de clientes
         self.clientes = []
+
+        # Lista de servicios
         self.servicios = []
+
+        # Lista de reservas
         self.reservas = []
 
+    # Registrar clientes
     def agregar_cliente(self, cliente):
+
         self.clientes.append(cliente)
 
+        logging.info(
+            f"Cliente agregado correctamente: {cliente.nombre}"
+        )
+
+    # Registrar servicios
     def agregar_servicio(self, servicio):
+
         self.servicios.append(servicio)
 
+        logging.info(
+            f"Servicio agregado: {servicio.nombre}"
+        )
+
+    # Registrar reservas
     def agregar_reserva(self, reserva):
+
         self.reservas.append(reserva)
 
+        logging.info(
+            f"Reserva registrada para: {reserva.cliente.nombre}"
+        )
+
+    # Registrar errores
     def registrar_error(self, error):
+
         logging.error(error)
