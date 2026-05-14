063c02e440293841a7e738fcf261b66744bf6562
Jhon Castelblanco <castelblancojhon075@gmail.com>
2026-05-14 00:49:03 -0500

    Se agregan comentarios y validaciones en Cliente_Jhon Castelblanco

--- cliente.py
+++ cliente.py
@@ -5,29 +5,24 @@ from excepciones import ClienteInvalidoError
 
 class Cliente(Entidad):
 
+    # Constructor de la clase cliente
     def __init__(self, id_cliente, nombre, correo):
+
+        # Llamado al constructor padre
         super().__init__(id_cliente)
 
+        # Validación del nombre
         if len(nombre.strip()) < 3:
             raise ClienteInvalidoError(
                 "El nombre del cliente es inválido."
             )
 
+        # Validación del correo
         if "@" not in correo:
             raise ClienteInvalidoError(
                 "El correo electrónico no es válido."
             )
 
+        # Encapsulación de atributos
         self.__nombre = nombre
         self.__correo = correo
-
-    @property
-    def nombre(self):
-        return self.__nombre
-
-    @property
-    def correo(self):
-        return self.__correo
-
-    def mostrar_info(self):
-        return f"Cliente: {self.__nombre} - {self.__correo}"
