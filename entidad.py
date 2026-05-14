feda025f387e7e6d8da45a8b4910ee44e85ad2b1
Jhon Castelblanco <castelblancojhon075@gmail.com>
2026-05-14 00:45:43 -0500

    Se documenta la clase abstracta Entidad_Jhon Castelblanco

--- entidad.py
+++ entidad.py
@@ -1,12 +1,15 @@
 from abc import ABC, abstractmethod
-
 # Clase abstracta principal del sistema
 
 class Entidad(ABC):
 
+    # Constructor principal
     def __init__(self, id_entidad):
+
+        # Atributo protegido
         self._id_entidad = id_entidad
 
+    # Método abstracto obligatorio
     @abstractmethod
     def mostrar_info(self):
         pass
